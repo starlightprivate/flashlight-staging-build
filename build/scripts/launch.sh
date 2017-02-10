18a5cfbb2e5182647fd9ac45cc794c8d63834f71
Evan Rusin <evan.rusin@outlook.com>
2017-02-11 00:47:24 +0200

    docker cache clean up

--- build/scripts/launch.sh
+++ build/scripts/launch.sh
@@ -1,3 +1,5 @@
+docker rm $(docker ps -a -q) || true
 docker kill $(docker ps -q) || true
 docker-compose build 
 docker-compose up -d
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
