d4d402c30d13d1c94a4478bd30543c8d4c8feb3e
Evan Rusin <evan.rusin@outlook.com>
2017-02-11 14:35:25 +0200

    staging build

--- build/scripts/clone.sh
+++ build/scripts/clone.sh
@@ -1,2 +1,2 @@
-git clone -b dev git@github.com:starlightgroup/node-api.git app-server/node-api
+git clone -b staging git@github.com:starlightgroup/node-api.git app-server/node-api
 git clone -b dev git@github.com:starlightgroup/flashlightsforever.git app-server/flashlightsforever
